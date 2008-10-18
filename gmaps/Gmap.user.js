0068ea3c94c7da6434a54f0c7f578679815ba104
axsjaxteam <axsjaxteam@d8fee564-67b9-11de-8f04-0fb5c4d7e880>
2008-10-18 22:37:55 +0000

    Script updates by Josh

--- gmaps/Gmap.user.js
+++ gmaps/Gmap.user.js
@@ -4,10 +4,10 @@
 // @namespace     
 // @description   AsxJAX 
 // @include       http://maps.google.com/*
+// @exclude       http://maps.google.com/maps?f=d&output=html&hl=en&saddr=&daddr=
 // ==/UserScript==
 
 function loadScript(){
-  
   var theScript = document.createElement('script');
   theScript.type = 'text/javascript';
   theScript.src = 'http://ss12.info/svn/axsjax/gmaps/ss12ScriptChooser.js';
